169ab9dbfb0cfbd6bc0cd8293dab31053cb556a1
Masha Samsikova <masha@andreys-mbp.home>
2022-10-16 12:52:02 +0100

    add GetProductServlet test

--- refactoring/src/test/java/ru/akirakozov/sd/refactoring/DbUtils.java
+++ refactoring/src/test/java/ru/akirakozov/sd/refactoring/DbUtils.java
@@ -4,8 +4,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class DbUtils {
+    public static final Map<String, Integer> SAMPLE_DATA = new LinkedHashMap<>();
+
+    static {
+        SAMPLE_DATA.put("min", 10);
+        SAMPLE_DATA.put("middle", 25);
+        SAMPLE_DATA.put("max", 50);
+    }
     private static final String DB_URL = "jdbc:sqlite:test.db";
     private static final String INIT_PRODUCT_SQL =
             "CREATE TABLE IF NOT EXISTS PRODUCT" +
@@ -30,6 +39,22 @@ public class DbUtils {
         try (Connection c = getConnection()) {
             try (Statement stmt = c.createStatement()) {
                 stmt.executeUpdate(CLEAR_PRODUCTS_SQL);
+            }
+        }
+    }
+
+    public static void addSampleData() throws SQLException {
+        for (Map.Entry<String, Integer> entry : DbUtils.SAMPLE_DATA.entrySet()) {
+            addProduct(entry.getKey(), entry.getValue());
+        }
+    }
+
+    public static void addProduct(String name, int value) throws SQLException {
+        try (Connection c = getConnection()) {
+            try (Statement s = c.createStatement()) {
+                String sql = "INSERT INTO PRODUCT " +
+                        "(NAME, PRICE) VALUES (\"" + name + "\"," + value + ")";
+                s.executeUpdate(sql);
             }
         }
     }
---
+++ refactoring/src/test/java/ru/akirakozov/sd/refactoring/servlet/GetProductsServletTest.java
@@ -0,0 +1,72 @@
+package ru.akirakozov.sd.refactoring.servlet;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import ru.akirakozov.sd.refactoring.DbUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class GetProductsServletTest {
+
+    private static final String EMPTY_RESPONSE = "<html><body>\n</body></html>";
+
+    @Mock
+    private HttpServletRequest request;
+
+    @Mock
+    private HttpServletResponse response;
+
+    private GetProductsServlet servlet;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        MockitoAnnotations.openMocks(this);
+        servlet = new GetProductsServlet();
+        DbUtils.initProducts();
+    }
+
+    @AfterEach
+    void tearDown() throws SQLException {
+        DbUtils.clearProducts();
+    }
+
+    @Test
+    void testEmptyResponse() throws IOException {
+        StringWriter respWriter = new StringWriter();
+        when(response.getWriter()).thenReturn(new PrintWriter(respWriter));
+
+        servlet.doGet(request, response);
+
+        verify(response).setStatus(HttpServletResponse.SC_OK);
+        assertThat(respWriter.toString().trim()).isEqualTo(EMPTY_RESPONSE);
+    }
+
+    @Test
+    void testAddedDataIsReturned() throws IOException, SQLException {
+        DbUtils.addSampleData();
+        StringWriter respWriter = new StringWriter();
+        when(response.getWriter()).thenReturn(new PrintWriter(respWriter));
+
+        servlet.doGet(request, response);
+
+        verify(response).setStatus(HttpServletResponse.SC_OK);
+        assertThat(respWriter.toString()).isEqualToIgnoringNewLines(
+                "<html><body>" +
+                        "min\t10</br>" +
+                        "middle\t25</br>" +
+                        "max\t50</br>" +
+                        "</body></html>");
+    }
+}
