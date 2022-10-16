54be2000f40e47f1f289998d3cae2b6fdb8034da
Masha Samsikova <masha@andreys-mbp.home>
2022-10-16 14:15:53 +0100

    Extract product insertion to DB in AddProductServlet

--- refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/AddProductServlet.java
+++ refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/AddProductServlet.java
@@ -1,12 +1,17 @@
 package ru.akirakozov.sd.refactoring.servlet;
 
+import ru.akirakozov.sd.refactoring.entity.Product;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
+
+import static ru.akirakozov.sd.refactoring.utils.CommonUtils.*;
 
 /**
  * @author akirakozov
@@ -15,23 +20,27 @@ public class AddProductServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String name = request.getParameter("name");
-        long price = Long.parseLong(request.getParameter("price"));
+        String name = request.getParameter(NAME_COLUMN);
+        long price = Long.parseLong(request.getParameter(PRICE_COLUMN));
 
         try {
-            try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                String sql = "INSERT INTO PRODUCT " +
-                        "(NAME, PRICE) VALUES (\"" + name + "\"," + price + ")";
-                Statement stmt = c.createStatement();
-                stmt.executeUpdate(sql);
-                stmt.close();
-            }
+            addProduct(new Product(name, price));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
-        response.setContentType("text/html");
+        response.setContentType(RESPONSE_CONTENT_TYPE);
         response.setStatus(HttpServletResponse.SC_OK);
         response.getWriter().println("OK");
+    }
+
+    private void addProduct(Product product) throws SQLException {
+        try (Connection c = DriverManager.getConnection(CONNECTION_URL)) {
+            String sql = "INSERT INTO PRODUCT " +
+                    "(NAME, PRICE) VALUES (\"" + product.getName() + "\"," + product.getPrice() + ")";
+            Statement stmt = c.createStatement();
+            stmt.executeUpdate(sql);
+            stmt.close();
+        }
     }
 }
